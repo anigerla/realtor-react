962498c1402c10ba97749282401c0d6dacdf2e39
anigerla <regina.zaripova@gmail.com>
2022-12-29 13:47:30 -0500

    Router setup & cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import Offers from './pages/Offers'
 
 function App() {
   return (
     <>
-      <h1>Yep</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/Offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
