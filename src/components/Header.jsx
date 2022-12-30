a5dd3d69c114cda103e2a4d1aa429bed36c747c3
anigerla <regina.zaripova@gmail.com>
2022-12-30 12:34:35 -0500

    Sing in input fields setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
       <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
-        <img src='logo.svg' alt="" className="h-32 cursor-pointer" onClick={()=> navigate("/")}/>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BoNgYCU0sSObXA4npMohm5_FMvk6DDW9byH87EAMN2no7N2f0uRt3LO5apuxH4tTKw&usqp=CAU' alt="" className="h-4 cursor-pointer" onClick={()=> navigate("/")}/>
         <div>
           <ul className='flex space-x-10'>
             <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-600 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
