11aba844e2a739ab737c27f6e797db7e25bcc95c
anigerla <regina.zaripova@gmail.com>
2023-01-06 15:31:24 -0500

    Send message functionality & styling component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,17 +8,23 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper"
 import "swiper/css/bundle"
 import { FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa"
+import { getAuth } from "firebase/auth" 
+import Contact from "../components/Contact"
 
 export default function Listing() {
+  const auth = getAuth()
   const params = useParams()
   const [listing, setListing] = useState(null)
   const [loading, setLoading] = useState(true)
   const [shareLinkCopied, setShareLinkCopied] = useState(false)
+  const [contactLandlord, setContactLandlord] = useState(false)
+
   SwiperCore.use([
     Autoplay, 
     Navigation, 
     Pagination
   ])
+
   useEffect(() =>{
     async function fetchListing() {
         const docRef = doc(db, "listings", params.listingId)
@@ -30,9 +36,11 @@ export default function Listing() {
     }
     fetchListing()
   }, [params.listingId])
+
   if(loading) {
     return <Spinner />
   }
+   
   return <main>
     <Swiper
         slidesPerView={1}
@@ -72,7 +80,7 @@ export default function Listing() {
         </p>
     )}
     <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
-        <div className="w-full h-[200px] lg-[400px]">
+        <div className="w-full">
             <p className="text-2xl font-bold mb-3 text-blue-900">
                 {listing.name} - $ {listing.offer ? listing.discountedPrice
                         .toString()
@@ -91,19 +99,19 @@ export default function Listing() {
                 <p className="bg-red-800 w-full max-w-[200] rounded-md p-1 text-white text-center font-semibold shadow-md">
                     {listing.type === "rent" ? "Rent" : "Sale"}
                 </p>
-                <p className="bg-green-800 w-full max-w-[200] rounded-md p-1 text-white text-center font-semibold shadow-md">
-                    {listing.offer && (
-                        <p>
+                {listing.offer && (
+                    <p className="bg-green-800 w-full max-w-[200] rounded-md p-1 text-white text-center font-semibold shadow-md">
+                        <span>
                             $ {listing.regularPrice - listing.discountedPrice} discount
-                        </p>
-                    )}
-                </p>
+                        </span>
+                    </p>
+                )}
             </div>
             <p className="mt-3 mb-3">
                 <span className="font-semibold">Description -  </span>
                 {listing.description}
             </p>
-            <ul className="flex items-center space-x-2 lg:space-x-10 text-sm font-semibold">
+            <ul className="flex items-center space-x-2 lg:space-x-10 text-sm font-semibold mb-6">
                 <li className="flex items-center whitespace-nowrap">
                     <FaBed className="text-lg mr-1"/>
                     {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : `${listing.bedrooms} Bed`}
@@ -121,6 +129,22 @@ export default function Listing() {
                     {listing.furnished ? `furnished` : `not furnished`}
                 </li>
             </ul>
+            {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+                <div className="mt-6">
+                    <button 
+                        className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out"
+                        onClick={() => setContactLandlord(true)}
+                    >
+                        Contact the Landlord
+                    </button>
+                </div>
+            )}
+            {contactLandlord && (
+                <Contact 
+                    userRef={listing.userRef}
+                    listing={listing}
+                />
+            )}
         </div>
         <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
     </div>
